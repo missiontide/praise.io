18b350f6c177d07c8b0537fcef306e68d80f701f
missiontide <patcoronel@missiontide.com>
2022-05-12 11:43:04 -0700

    made App stateful and moved songs state to App.
    addsong button now lifts song.id up to App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import React from 'react';
 import SongSearchBar from "./SongSearchBar";
 
-function App() {
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            songs: [],
+        }
+    }
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <p>
-                    Search for a worship song:
-                </p>
-            </header>
-            <SongSearchBar />
-        </div>
-    );
+    // get song data from api
+    componentDidMount() {
+        // function to make GET request to server for api
+        const getData = async () => {
+            const response = await fetch("/songs");
+            const data = await response.json();
+
+            this.setState({songs: data});
+            this.render();
+        };
+
+        getData().then()
+    }
+
+    // handles add song click
+    handleClick(id) {
+        console.log(id);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        Search for a worship song:
+                    </p>
+                </header>
+                <SongSearchBar songs={this.state.songs} onClick={(id) => this.handleClick(id)}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/SongSearchBar.js
+++ src/SongSearchBar.js
@@ -8,21 +8,7 @@ class SongSearchBar extends React.Component {
         this.handleChange = this.handleChange.bind(this);
         this.state = {
             searchInput: "",
-            songs: [],
         }
-    }
-
-    // get song data from api
-    componentDidMount() {
-        // function to make GET request to server for api
-        const getData = async () => {
-            const response = await fetch("/songs");
-            const data = await response.json();
-
-            this.setState({songs: data});
-        };
-
-        getData().then();
     }
 
     handleChange(e) {
@@ -38,7 +24,7 @@ class SongSearchBar extends React.Component {
 
         if (this.state.searchInput.length > 0) {
             // match song titles/artists with search input
-            songsToDisplay = this.state.songs.filter((song) => {
+            songsToDisplay = this.props.songs.filter((song) => {
                 return song.title.toLowerCase().match(this.state.searchInput.toLowerCase())
                     || song.artist.toLowerCase().match(this.state.searchInput.toLowerCase());
             })
@@ -66,7 +52,10 @@ class SongSearchBar extends React.Component {
                                 <tr key={index}>
                                     <td>{song.title}</td>
                                     <td>{song.artist}</td>
-                                    <td><Button variant="warning">+</Button></td>
+                                    <td><AddSongButton
+                                        value={song.id}
+                                        onClick={() => this.props.onClick(song.id)}
+                                    /></td>
                                 </tr>
                             )
                         })}
@@ -76,6 +65,12 @@ class SongSearchBar extends React.Component {
             </div>
         )
     }
+}
+
+function AddSongButton(props) {
+    return (
+        <Button variant="warning" onClick={props.onClick}>+</Button>
+    )
 }
 
 export default SongSearchBar;
