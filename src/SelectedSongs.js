9563d91ae62c5836c89c4d0a4b4f20a16e31cad1
missiontide <patcoronel@missiontide.com>
2022-05-28 17:27:08 -0700

    refactored SelectedSongs to function component
    simplified Make Slides button logic

--- src/SelectedSongs.js
+++ src/SelectedSongs.js
@@ -1,89 +1,58 @@
-import React from 'react';
 import { Offcanvas, Button, Card, ListGroup } from "react-bootstrap";
 
-class SelectedSongs extends React.Component {
+export default function SelectedSongs(props) {
+    const noSongsSelected = props.selectedSongs.length === 0
+    
+    return (
+        <>
+            {/* top-left button: open offCanvas */}
+            <Button className="makeSlidesButton" variant="dark" onClick={props.onShow}>
+                Make Slides
+            </Button>
 
-    render() {
-        let makeSlidesButton;
-        const noSongsSelected = this.props.selectedSongs.length === 0
-        // slidesCreated is an App state set when a ppt has just been made
-        // -- disable button until user makes a change
-        if (this.props.slidesCreated === false && !noSongsSelected) {
-            makeSlidesButton = (
-                <Button variant="primary" style={{height: '6rem'}}
-                        className="float-end fixed-bottom position-absolute"
-                        onClick={() => this.props.makeSlides()}
-                >
-                    Make Slides
-                </Button>
-            );
-        } else if (this.props.slidesCreated === false && noSongsSelected){
-            makeSlidesButton = (
-                <Button variant="primary" style={{height: '6rem'}}
-                        className="float-end fixed-bottom position-absolute"
-                        disabled
-                >
-                    Make Slides
-                </Button>
-            );
-        } else if (this.props.slidesCreated === true) {
-            makeSlidesButton = (
-                <Button variant="success" style={{height: '6rem'}}
-                        className="float-end fixed-bottom position-absolute"
-                        disabled
-                >
-                    Slides Downloaded!
-                </Button>
-            );
-        }
-
-        return (
-            <>
-                <Button className="makeSlidesButton" variant="dark" onClick={this.props.onShow}>
-                    Make Slides
-                </Button>
-
-                <Offcanvas show={this.props.show} onHide={this.props.onHide}>
-                    <Offcanvas.Header closeButton>
-                        <Offcanvas.Title>Slide Maker</Offcanvas.Title>
-                    </Offcanvas.Header>
-                    <Offcanvas.Body>
-                        <Card style={{ width: '22.5rem' }}>
-                            <Card.Header>{noSongsSelected ? "Selected Songs" : "Select a song"}</Card.Header>
-                            {/* song list */}
-                            <ListGroup variant="flush">
-                                {this.props.selectedSongs.map((song, index) => {
-                                    return (
-                                        <ListGroup.Item key={index + "-" + song._id}>
-                                            {song.title} - {song.artist}
-                                            <Button
-                                                variant="outline-danger" size="sm" className="float-sm-end"
-                                                onClick={() => this.props.onClick(index)}
-                                            >
-                                                x
-                                            </Button>
-                                        </ListGroup.Item>
-                                    )
-                                })}
-                            </ListGroup>
-                        </Card>
-                        {/* Add more songs button (closes Offcanvas) */}
-                        {this.props.selectedSongs.length < 10 &&
-                            <Button variant="dark"
-                                    style={{"margin-top": "10px", "margin-left": "12rem"}}
-                                    onClick={this.props.onHide}
-                            >
-                                {noSongsSelected ? "Add a song >" : "Add more songs >"}
-                            </Button>
-                        }
-                        {/* make slides button */}
-                        {makeSlidesButton}
-
-                    </Offcanvas.Body>
-                </Offcanvas>
-            </>
-        )
-    }
+            <Offcanvas show={props.show} onHide={props.onHide}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Slide Maker</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    <Card style={{ width: '22.5rem' }}>
+                        <Card.Header>{noSongsSelected ? "Selected Songs" : "Select a song"}</Card.Header>
+                        {/* song list */}
+                        <ListGroup variant="flush">
+                            {props.selectedSongs.map((song, index) => {
+                                return (
+                                    <ListGroup.Item key={index + "-" + song['_id']}>
+                                        {song.title} - {song.artist}
+                                        <Button
+                                            variant="outline-danger" size="sm" className="float-sm-end"
+                                            onClick={() => props.onClick(index)}
+                                        >
+                                            x
+                                        </Button>
+                                    </ListGroup.Item>
+                                )
+                            })}
+                        </ListGroup>
+                    </Card>
+                    {/* Add more songs button (closes Offcanvas) */}
+                    {props.selectedSongs.length < 10 &&
+                        <Button variant="dark"
+                                style={{"marginTop": "10px", "marginLeft": "12rem"}}
+                                onClick={props.onHide}
+                        >
+                            {noSongsSelected ? "Add a song >" : "Add more songs >"}
+                        </Button>
+                    }
+                    {/* make slides button */}
+                    <Button variant="primary" style={{height: '6rem'}}
+                            className="float-end fixed-bottom position-absolute"
+                            onClick={() => props.makeSlides()}
+                            disabled={noSongsSelected || props.slidesCreated === true}
+                    >
+                        {props.slidesCreated === false ? "Make Slides" : "Slides Downloaded!"}
+                    </Button>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </>
+    )
 }
-
-export default SelectedSongs;
